eb0e6b894eacc2374fee2e6b45f4c01df4e6e86e
vheissu <dwaynecharrington@gmail.com>
2016-12-22 14:33:16 +1000

    feat(services): mock users now have permissions

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,63 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 
-const DEMO_MODE = true;
+export const DEMO_MODE = true;
 
-let users = [
-    {id: 34234, image: '/images/avatars/big-pete-wrigley.jpg', name: 'Big Pete Wrigley', username: 'petewrigley', email: 'bigpete@gmail.com', password: 'superhardpassword123'},
-    {id: 938493, image: '/images/avatars/tina-carlyle.jpg', name: 'Tina Carlyle', username: 'tina', email: 'tina.carlyle@gmail.com', password: 'ysoserious?'},
-    {id: 991, image: '/images/avatars/tre-styles.jpg', name: 'Tre Styles', username: 'trestyles123', email: 'trestylessuperfly@gmail.com', password: 'toomuchmoneytocount'},
-    {id: 1, image: '/images/avatars/cameron-frye.jpg', name: 'Cameron Frye', username: 'cameron', email: 'cameronfrye@frye.com', password: 'kjfkjkdjf'},
-    {id: 42, image: '/images/avatars/stoney-brown.jpg', name: 'Stoney Brown', username: 'stoney', email: 'stoney@gmail.com', password: 'password'},
-    {id: 721124, image: '/images/avatars/will-smith.jpg', name: 'Will Smith', username: 'bigwillystyle', email: 'bigwill@gmail.com', password: 'f222'}
+export let users = [
+    {
+        id: 34234, 
+        image: '/images/avatars/big-pete-wrigley.jpg', 
+        name: 'Big Pete Wrigley', 
+        username: 'petewrigley', 
+        email: 'bigpete@gmail.com', 
+        password: 'superhardpassword123',
+        permissions: ['super-admin']
+    },
+    {
+        id: 938493, 
+        image: '/images/avatars/tina-carlyle.jpg', 
+        name: 'Tina Carlyle', 
+        username: 'tina', 
+        email: 'tina.carlyle@gmail.com', 
+        password: 'ysoserious?',
+        permissions: ['create', 'read', 'edit-pending-approval']
+    },
+    {
+        id: 991, 
+        image: '/images/avatars/tre-styles.jpg', 
+        name: 'Tre Styles', 
+        username: 'trestyles123', 
+        email: 'trestylessuperfly@gmail.com', 
+        password: 'toomuchmoneytocount',
+        permissions: ['read']
+    },
+    {
+        id: 1, 
+        image: '/images/avatars/cameron-frye.jpg',
+        name: 'Cameron Frye', 
+        username: 'cameron', 
+        email: 'cameronfrye@frye.com', 
+        password: 'kjfkjkdjf',
+        permissions: ['create', 'read']
+    },
+    {
+        id: 42, 
+        image: '/images/avatars/stoney-brown.jpg', 
+        name: 'Stoney Brown', 
+        username: 'stoney', 
+        email: 'stoney@gmail.com', 
+        password: 'password',
+        permissions: ['read', 'edit-pending-approval']
+    },
+    {
+        id: 721124, 
+        image: '/images/avatars/will-smith.jpg', 
+        name: 'Will Smith', 
+        username: 'bigwillystyle', 
+        email: 'bigwill@gmail.com', 
+        password: 'f222',
+        permissions: ['read']
+    }
 ];
 
 export class Api {
