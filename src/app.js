02033a47afaa2864ec32284bec6efdc8427ffdf5
vheissu <dwaynecharrington@gmail.com>
2016-12-22 12:16:09 +1000

    feat(app): additional routes

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ export class App {
 
         config.map([
             { route: '', redirect: 'user/login' },
-            { route: 'auth/login', name: 'login', layoutView: 'views/layouts/auth.html', moduleId:'./views/auth/login', nav: true,  title: 'Login' },
-            { route: 'dashboard', name: 'dashboard', moduleId:'./views/dashboard/home', nav: true,  title: 'Dashboard' }
+            { route: 'auth/login', name: 'login', layoutView: 'views/layouts/auth.html', moduleId:'./views/auth/login', nav: false,  title: 'Login' },
+            { route: 'dashboard', name: 'dashboard', moduleId:'./views/dashboard/home', nav: true,  title: 'Dashboard' },
+            { route: 'dashboard/users', name: 'users', moduleId:'./views/dashboard/users/list', nav: true,  title: 'Users' },
+            { route: 'dashboard/users/:id', name: 'usersView', moduleId:'./views/dashboard/users/view', nav: false,  title: 'User' }
         ]);
     }
 }
