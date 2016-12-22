e9490341f189725e2cfe6c97314194d17a270b61
vheissu <dwaynecharrington@gmail.com>
2016-12-22 15:40:11 +1000

    feat(login): get avatar for username (if found) and animations for logging in

--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -1,5 +1,20 @@
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+
+import {CssAnimator} from 'aurelia-animator-css';
+
+import {User} from '../../services/user';
+
+@inject(User, Router, CssAnimator)
 export class Login {
-    constructor() {
+    
+    constructor(user, router, animator) {
+        this.user = user;
+        this.router = router;
+        this.animator = animator;
+
+        this.foundAvatar = null;
+
         this.username = '';
         this.password = '';
     }
@@ -12,7 +27,15 @@ export class Login {
         document.body.classList.remove('login-screen');
     }
 
+    findUserAvatar() {
+        this.user.findAvatar(this.username).then(avatar => this.foundAvatar = avatar);
+    }
+
     handleFormSubmission(e) {
-        
+        this.user.login(this.username.trim(), this.password)
+            .then(() => { 
+                this.animator.addClass(this.loginForm, 'fade-out').then(() => this.router.navigateToRoute('dashboard'));
+            })
+            .catch(e => this.errors = e);
     }
 }
