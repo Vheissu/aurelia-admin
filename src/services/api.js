71da90475e3f3e0f3a592ee34c36f7c8c495ed8d
vheissu <dwaynecharrington@gmail.com>
2016-12-22 12:16:00 +1000

    feat(api): get user method

--- src/services/api.js
+++ src/services/api.js
@@ -19,4 +19,8 @@ export class Api {
     getUsers() {
         return Promise.resolve(users);
     }
+
+    getUser(id) {
+        return Promise.resolve(users.filter(user => user.id === Number(id)));
+    }
 }
