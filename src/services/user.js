a83b38792d4db139b0c8781f2016fd51e02b3be4
vheissu <dwaynecharrington@gmail.com>
2016-12-22 14:32:56 +1000

    feat(services): groundwork on user singleton for login/logout/permissions

--- src/services/user.js
+++ src/services/user.js
@@ -5,6 +5,39 @@
  * 
  */
 
+import {users, Api} from './api';
+
 export class User {
 
+    constructor() {
+        this.loggedIn = false;
+        this.currentUser = {};
+    }
+
+    login(username, password) {
+        return new Promise((resolve, reject) => {
+            users.forEach(user => {
+                if (user.username === username) {
+                    if (user.password === password) {
+                        this.currentUser = user;
+                        this.loggedIn = true;
+                        return resolve(user);
+                    }
+                }
+            });
+
+            return reject(new Error('Credentials were invalid, please check your username and password then try again'));
+        });
+    }
+
+    logout() {
+        this.loggedIn = false;
+        this.currentUser = {};
+
+        return Promise.resolve();
+    }
+
+    userCan(permission) {
+        return this.currentUser.permissions.includes(permission);
+    }
 }
