41654e67e4ed7122b7e5856046f27fa658b82fd7
vheissu <dwaynecharrington@gmail.com>
2016-12-22 11:24:28 +1000

    feat(auth): login page and styles

--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -1,3 +1,18 @@
 export class Login {
-    
+    constructor() {
+        this.username = '';
+        this.password = '';
+    }
+
+    attached() {
+        document.body.classList.add('login-screen');
+    }
+
+    detached() {
+        document.body.classList.remove('login-screen');
+    }
+
+    handleFormSubmission(e) {
+        
+    }
 }
