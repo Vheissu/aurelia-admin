d8e066eafb3174ca82ba5d12525c364bbe34c9a8
vheissu <dwaynecharrington@gmail.com>
2016-12-22 14:02:45 +1000

    feat(app): mock user images and names

--- src/services/api.js
+++ src/services/api.js
@@ -4,11 +4,12 @@ import {HttpClient} from 'aurelia-fetch-client';
 const DEMO_MODE = true;
 
 let users = [
-    {id: 34234, name: 'Rob Schlob', username: 'rob', email: 'robdobjob@gmail.com', password: 'superhardpassword123'},
-    {id: 938493, name: 'George Clooney', username: 'clooney1', email: 'george.clooney@gmail.com', password: 'ysoserious?'},
-    {id: 991, name: 'Adam Sandler', username: 'badmoviemaker', email: 'adamsandlermakesmovies@gmail.com', password: 'toomuchmoneytocount'},
-    {id: 1, name: 'Elon Musk', username: 'elon', email: 'elonmusk@tesla.com', password: 'spacextothemoon11'},
-    {id: 42, name: 'Donald Trump', username: 'donaldtrump', email: 'donald@trump.com', password: 'password'}
+    {id: 34234, image: '/images/avatars/big-pete-wrigley.jpg', name: 'Big Pete Wrigley', username: 'petewrigley', email: 'bigpete@gmail.com', password: 'superhardpassword123'},
+    {id: 938493, image: '/images/avatars/tina-carlyle.jpg', name: 'Tina Carlyle', username: 'tina', email: 'tina.carlyle@gmail.com', password: 'ysoserious?'},
+    {id: 991, image: '/images/avatars/tre-styles.jpg', name: 'Tre Styles', username: 'trestyles123', email: 'trestylessuperfly@gmail.com', password: 'toomuchmoneytocount'},
+    {id: 1, image: '/images/avatars/cameron-frye.jpg', name: 'Cameron Frye', username: 'cameron', email: 'cameronfrye@frye.com', password: 'kjfkjkdjf'},
+    {id: 42, image: '/images/avatars/stoney-brown.jpg', name: 'Stoney Brown', username: 'stoney', email: 'stoney@gmail.com', password: 'password'},
+    {id: 721124, image: '/images/avatars/will-smith.jpg', name: 'Will Smith', username: 'bigwillystyle', email: 'bigwill@gmail.com', password: 'f222'}
 ];
 
 export class Api {
